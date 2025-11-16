c80cb94c88d218c743921c07b5bc18b32cd42159
Daniel Kpatamia <danielkpatamia15@gmail.com>
2025-11-16 10:35:19 +0000

    Refined Carousel with flex style

--- frontend/src/components/ui/carousel.tsx
+++ frontend/src/components/ui/carousel.tsx
@@ -10,7 +10,7 @@ import { CollectionProp, Collections } from "../../services/types";
 
 export const CollectionsCarousel = ({ collections }: Collections) =>{
   return (
-    <div className="w-full py-8">
+    <div className="w-full py-8 flex gap-1 flex-wrap md:flex-nowrap">
       <Swiper
         modules={[ Autoplay]}
         autoplay={{ delay: 5000 }}
@@ -29,7 +29,40 @@ export const CollectionsCarousel = ({ collections }: Collections) =>{
                 />
           </SwiperSlide>
         ))}
-      </Swiper>
+          </Swiper>
+          <section className="lg:w-1/3 bg-white/4 backdrop-blur-lg border border-white/30 rounded-xl p-6 shadow-lg flex flex-col gap-4">
+        <div className="mt-4 flex flex-col gap-3">
+          {/* Best For */}
+          <div className="flex justify-between items-center p-4 rounded-xl bg-white/20 backdrop-blur-lg border border-white/30 shadow-md">
+            <span className="text-white font-bold text-sm">Best For</span>
+            <span className="text-white/80 text-sm">Occasional Gift</span>
+          </div>
+
+          {/* Rarity */}
+          <div className="flex justify-between items-center p-4 rounded-xl bg-white/20 backdrop-blur-lg border border-white/30 shadow-md">
+            <span className="text-white font-bold text-sm">Rarity</span>
+            <span className="text-white/80 text-sm text-right">
+              Unique 1/1 & Limited Editions
+            </span>
+          </div>
+
+          {/* Holders */}
+          <div className="flex justify-between items-center p-4 rounded-xl bg-white/20 backdrop-blur-lg border border-white/30 shadow-md">
+            <span className="text-white font-bold text-sm">Holders</span>
+            <span className="text-white/80 text-sm text-right">
+              12.3k collectors
+            </span>
+          </div>
+
+          {/* Activity */}
+          <div className="flex justify-between items-center p-4 rounded-xl bg-white/20 backdrop-blur-lg border border-white/30 shadow-md">
+            <span className="text-white font-bold text-sm">Activity</span>
+            <span className="text-white/80 text-sm text-right">
+              Avg. 150 transactions/day
+            </span>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
