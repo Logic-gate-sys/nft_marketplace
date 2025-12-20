70bf3fa29bd31daf14bc81400542fdb46d3d7998
Daniel Kpatamia <danielkpatamia15@gmail.com>
2025-12-20 00:30:03 +0000

    added proper ts build commands for the backend

---
+++ backend/src/controller/nft.js
@@ -0,0 +1,63 @@
+import { upload_nftMetaData_pinata, uploadImageToPinata } from "../utils/ifpfs";
+import { prisma } from './../lib/prisma';
+export const mintNFT = async (req, res) => {
+    try {
+        //retrieve fields needed form minting
+        const { col_id, token_id, owner_id, nft_uri } = req.body;
+        //----------------- onchain data upload to DB ------------------
+        const result = await prisma.nft.create({
+            data: {
+                token_id: Number(token_id),
+                col_id: Number(col_id),
+                uri: nft_uri,
+                owner_id: Number(owner_id)
+            }
+        });
+        if (!result) {
+            return res.json({ message: "Could not upload On-chaim mint data to DB" }).status(404);
+        }
+        return res.json(result);
+    }
+    catch (err) {
+        console.error(err);
+        return;
+    }
+};
+export const uploadMintOffChainData = async (req, res) => {
+    const { attributes } = req.body;
+    const file = req.file;
+    //----------- Off-chain minting ------------------------------------
+    if (!file) {
+        return res.json({ error: "No file provided ! " });
+    }
+    try {
+        // get file details
+        const { originalname, mimetype } = file;
+        const image_cid = await uploadImageToPinata(file.buffer, originalname, mimetype);
+        // if file upload fails
+        if (!image_cid) {
+            return res.status(403).json({
+                success: false,
+                message: "File uplaod to pinata failed"
+            });
+        }
+        //upload metadata to pinata
+        const tokenURI = await upload_nftMetaData_pinata(image_cid, attributes);
+        if (!tokenURI) {
+            return res.status(403).json({
+                success: false,
+                error: 'NFT metadata upload failed'
+            });
+        }
+        // return token url
+        return res.status(201).json({
+            success: true,
+            tokenURI: tokenURI,
+            message: "Metadata uploaded to pinata successfully"
+        });
+    }
+    catch (e) {
+        console.log("Error", e.message);
+        return;
+    }
+};
---
+++ backend/src/route/collectionRoute.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const { Router } = express;
+import { create_offchain_collection, fetchAllCollections, fetchCollectionABI, fetchCollectionById, fetchUserCollections } from '../controller/collection';
+import { Authenticate } from '@/middlewares/Auth';
+import multer from 'multer';
+const collectionRouter = Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+//endpoint for file upload http://localhost:3000/api/collections
+collectionRouter.post('/create', Authenticate, upload.array('files', 2), create_offchain_collection);
+// fetching collections 
+collectionRouter.get('/', fetchAllCollections);
+collectionRouter.get('/:col_id', fetchCollectionById);
+// 
+collectionRouter.get('/usercollection/user', Authenticate, fetchUserCollections);
+// Collection ABI
+collectionRouter.get('/collection/abi', Authenticate, fetchCollectionABI);
+export default collectionRouter;
