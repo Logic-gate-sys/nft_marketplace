e6c9493e52eb2fe4b7dbc6fc749e0294c463a9b0
Daniel Kpatamia <danielkpatamia15@gmail.com>
2026-03-06 22:38:29 +0000

    Ready to start refactoring

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import app from './app.ts';
 import { startMarketPlaceListeners } from './blockchain/marketplace.listener.ts';
 
@@ -8,18 +7,4 @@ let PORT = process.env.PORT || 3000;
 app.listen(PORT, async() => {
     console.log(`Server running on : http://localhost:${PORT}`);
     await startMarketPlaceListeners();
-=======
-import app from './app';
-import { startMarketPlaceListeners } from './blockchain';
-
-let PORT = process.env.PORT;
-
-// start event Listeners 
-
-app.listen(PORT, async() => {
-    console.log(`Server running on : http://localhost:${PORT}`);
-
-    await startMarketPlaceListeners();
-
->>>>>>> db7aaecd5b66d9beab530d02b1fe7dfd501dd197
 })
